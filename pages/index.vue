caa77086a3a794aaa9642a035ea64f586f316c7b
srdokinch <srdokinch@gmail.com>
2022-10-16 12:50:28 +0900

    Todoアプリコード追加

---
+++ pages/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <main class="d-flex">
+    <h1>Task Board</h1>
+    <p>Create a new task</p>
+
+    <div class="create-new">
+      <input 
+        type="text" 
+        v-model="newTask" 
+        placeholder="Add a new task" 
+        @keypress.enter="addTask">
+      <button @click="addTask">ADD</button>
+    </div>
+
+    <div class="tasks">
+      <Task 
+        v-for="(task, i) in $store.state.tasks" 
+        :key="i" 
+        :task="task" />
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newTask: ''
+    }
+  },
+  methods: {
+    addTask() {
+      if(this.newTask) {
+        this.$store.commit('ADD_TASK', this.newTask)
+        this.newTask = ''
+      }
+    }
+  }
+}
+</script>
